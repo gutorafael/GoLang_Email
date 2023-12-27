b841160c701f61d141bf40ec52551d5d0b1825c2
gu058358 <gu058358@intelbras.combr>
2023-12-27 09:32:42 -0300

    feat(service)new validations tests

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -10,8 +10,13 @@ type Service struct {
 
 func (s *Service) Create(newCampaign contract.NewCampaignDTO) (string, error) {
 	
-	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
-	s.Repository.Save(campaign)	
-
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	if err != nil {
+		return "", err
+	}
+	err = s.Repository.Save(campaign)
+	if err != nil {
+		return "", err
+	}	
 	return campaign.ID, nil
 } 
--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -2,29 +2,21 @@ package campaign
 
 import (
 	"email/internal/contract"
+	"errors"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 var (
-	service = Service{}
+	service     = Service{}
 	newCampaign = contract.NewCampaignDTO{
-		Name: "My Campaign",
+		Name:    "My Campaign",
 		Content: "My Content",
-		Emails: []string{"teste1@test.com"},
+		Emails:  []string{"teste1@test.com"},
 	}
 )
-
-func Test_Create_Campaign(t *testing.T) {
-	assert := assert.New(t)
-	
-	
-	id, err := service.Create(newCampaign)
-
-	assert.NotNil(id)
-	assert.Nil(err)
-}
 
 type MockRepository struct {
 	mock.Mock
@@ -34,26 +26,49 @@ func (m *MockRepository) Save(campaign *Campaign) error {
 	args := m.Called(campaign)
 	return args.Error(0)
 }
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	assert.Nil(err)
+	assert.Equal(name, campaign.Name)
+
+}
 
 func Test_Create_SaveCampaign(t *testing.T) {
-	assert := assert.New(t)
-	
 	repositoryMock := new(MockRepository)
+	service.Repository = repositoryMock
 	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
-		if campaign.Name != newCampaign.Name {
+		if campaign.Name != newCampaign.Name ||
+			campaign.Content != newCampaign.Content ||
+			len(campaign.Contacts) != len(newCampaign.Emails) {
 			return false
-		} else if campaign.Content != newCampaign.Content { 
-			return false
-		} else if len(campaign.Contacts) != len(newCampaign.Emails) {
-			return false
-		} 
+		}
 		return true
 	})).Return(nil)
-	service.Repository = repositoryMock
-	
-	id, err := service.Create(newCampaign)
 
-	assert.NotNil(id)
-	assert.Nil(err)
+	service.Create(newCampaign)
+
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateRepo(t *testing.T) {
+	assert := assert.New(t)
+	repositoryMock := new(MockRepository)
+	service.Repository = repositoryMock
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+
+	_, err := service.Create(newCampaign)
+	repositoryMock.AssertExpectations(t)
+	assert.Equal(err.Error(), "error to save on database")
+}
+
+func Test_Create_ValidateDomain(t *testing.T) {
+	assert := assert.New(t)
+	newCampaign.Name = ""
+
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal(err.Error(), "name is required")
 }
